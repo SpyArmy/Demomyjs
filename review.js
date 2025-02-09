625a0d41fd0d5a05b4ce86f276beaab8313abc34
Bryson Jensen <143565292+SpyArmy@users.noreply.github.com>
2025-02-09 15:03:57 -0700

    Create review.js

---
+++ review.js
@@ -0,0 +1,7 @@
+export const review = [
+	{ "name": "Seth Roth", "comment": "text", "date": "1/5/2024"}
+	{ "name": "Sam Adams", "comment": "text", "date": "2/23/2024"}
+	{ "name": "Dave Pork", "comment": "text", "date": "6/27/2023"}
+	{ "name": "John James", "comment": "text", "date": "4/18/2022"}
+
+]
